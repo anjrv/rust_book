139254bbf86544f7cc646ca5a483a03e176e991e
Anna Jaerving <AnnaJrving@gmail.com>
2022-06-30 18:11:37 +0000

    generics and traints 0.1

---
+++ generics/src/main.rs
@@ -0,0 +1,41 @@
+fn largest_number(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// T: ... define required traits that a passed in type needs to implement
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_number(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest_number(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
